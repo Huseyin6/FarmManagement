179f3c4b1f809c15f86932a5696e04614ff4b281
Huseyin6 <hsynsimsek723@gmail.com>
2019-09-20 10:48:49 +0300

    Edit

--- Farm.Data/Entities/Cattle.cs
+++ Farm.Data/Entities/Cattle.cs
@@ -14,7 +14,7 @@ namespace Farm.Data.Entities
         public int Id { get; set; }
         public int StateId { get; set; }  // In Farm, Sale, Died ...
         public string EaringNo { get; set; }// TR 06 1234
-        public DateTime? BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Name { get; set; }
         public int CattleTypeId { get; set; }
         public bool IsPregnant { get; set; }
